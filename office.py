bdcfb5d6711d62a84106fb98357c6e83523ea91c
Justinzenki <jgarcia02@sfo.yearup.org>
2016-01-21 09:51:09 -0800

    new

--- office.py
+++ office.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 print('Input your user name')
 userEmail = input()
 print('Input your password')
+userPassword = input()
 
 browser = webdriver.Firefox()
 browser.get('https://login.microsoftonline.com/')
@@ -12,5 +13,10 @@ emailElem.send_keys(userEmail)
 
 passwordElem = browser.find_element_by_id('cred_password_inputtext')
 passwordElem.send_keys(userPassword)
+passwordElem.submit()
 
+shellElem = browser.find_element_by_id('ShellMail_link_text')
+shellElem.click()
 
+newElem = browser.find_element_by_xpath('//*[@id="primaryContainer"]/div[4]/div/div[1]/div[2]/div[3]/div[2]/div/div/div[1]/div/div[2]/button')
+newElem.click()
